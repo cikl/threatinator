c58d8963c493366ddc2a247cf9e081e716cc20e6
justfalter <falter@gmail.com>
2014-06-13 16:31:51 -0500

    Add a Getline parser

---
+++ lib/threatinator/parsers/getline.rb
@@ -0,0 +1,40 @@
+module Threatinator
+  module Parsers
+    # Parses an IO, yielding each 'line' of data as deliniated by :separator.
+    # The text matching :separator will be included.
+    class Getline
+      # @param [IO, IOChain] io The IO from which we will read.
+      # @param [Hash] opts 
+      # @option opts [String] :separator ("\n") A character that will be used
+      #  to detect the end of a line.
+      def initialize(io, opts = {})
+        @io = io
+        @separator = opts[:separator] || "\n"
+
+        unless @separator.length == 1
+          raise ArgumentError.new(":separator must be exactly one character long")
+        end
+      end
+
+      def each
+        return enum_for(:each) unless block_given?
+
+        buffer = ""
+        while char = @io.read(1)
+          buffer << char
+
+          if buffer.end_with?(@separator)
+            yield buffer
+            buffer = ""
+          end
+        end
+
+        if buffer.length > 0
+          # Emit the last line before returning.
+          yield buffer
+        end
+        nil
+      end
+    end
+  end
+end
---
+++ spec/parsers/getline_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'threatinator/parsers/getline'
+require 'stringio'
+
+describe Threatinator::Parsers::Getline do
+  it "should default to newline as a separator" do
+    str = "line 1\nline 2\nline3!"
+    sio = StringIO.new(str)
+    parser = described_class.new(sio)
+
+    expect { |b|
+      parser.each(&b)
+    }.to yield_successive_args("line 1\n", "line 2\n", "line3!")
+  end
+  it "should let you specify a separator" do
+    str = "line 1\0line 2\0line3!"
+    sio = StringIO.new(str)
+    parser = described_class.new(sio, :separator => "\0")
+
+    expect { |b|
+      parser.each(&b)
+    }.to yield_successive_args("line 1\0", "line 2\0", "line3!")
+  end
+
+  it "shouldn't split anything up if the separator isn't found" do
+    str = "line 1\0line 2\0line3!"
+    sio = StringIO.new(str)
+    parser = described_class.new(sio, :separator => "\n")
+
+    expect { |b|
+      parser.each(&b)
+    }.to yield_successive_args(str)
+  end
+
+  it "should raise an ArgumentError if the separator isn't a single character" do
+    sio = StringIO.new()
+    expect { described_class.new(sio, :separator => "asdf") }.to raise_error(ArgumentError)
+  end
+end
