d4cac23b7d6b641d7ed8dd88fa1394303f30f689
justfalter <falter@gmail.com>
2014-06-13 13:03:36 -0500

    Clarified some IOWrapper stuff

--- spec/support/shared_examples_for_an_iowrapper.rb
+++ spec/support/shared_examples_for_an_iowrapper.rb
@@ -2,44 +2,61 @@ require 'spec_helper'
   shared_examples_for "an iowrapper" do
     it_should_behave_like "an iowrapper instance"
 
-    describe "when wrapping an existing IO Wrapper" do
+    describe "when wrapping an IOWrapper object" do
       it_should_behave_like "an iowrapper instance" do
         let(:wrapped_io) { 
-          r, w = IO.pipe()
-          w.write(expected_data)
-          w.close
-          Threatinator::IOWrapper.new(r)
+          Threatinator::IOWrapper.new(real_io_object)
         }
       end
     end
   end
 
   shared_examples_for "an iowrapper instance" do
-    let(:expected_data) { "here's some data" }
-    let(:wrapped_io) { 
+    let(:input_data)    { "here's some data" }
+    let(:expected_data) { input_data }
+
+    # The real IO object that is ultimately wrapped.
+    let(:real_io_object) { 
       r, w = IO.pipe()
-      w.write(expected_data)
+      w.write(input_data)
       w.close
       r
     }
+
+    # The IO or IOWrapper that is wrapped by 'io_wrapper'. This is just so that
+    # we can test that the described_class can operate on wrapped IO objects,
+    # as well as real IO objects. 
+    let(:wrapped_io) { real_io_object }
     let!(:io_wrapper) { described_class.wrap(wrapped_io) }
 
     after :each do
       io_wrapper.close rescue nil
-      wrapped_io.to_io.close rescue nil
+      real_io_object.close rescue nil
     end
 
     describe "#to_io" do
-      it "should return the the wrapped io object" do
-        expect(io_wrapper.to_io).to be(wrapped_io.to_io)
+      it "should return the the real io object" do
+        expect(io_wrapper.to_io).to be(real_io_object)
       end
     end
 
     describe "#close" do
-      it "should close the wrapped io object" do
-        expect(wrapped_io.to_io).not_to be_closed
+      it "should close the real io object" do
+        expect(real_io_object).not_to be_closed
         io_wrapper.close
-        expect(wrapped_io.to_io).to be_closed
+        expect(real_io_object).to be_closed
+      end
+
+      it "should close the wrapped io object" do
+        expect(wrapped_io).not_to be_closed
+        io_wrapper.close
+        expect(wrapped_io).to be_closed
+      end
+
+      it "should close the io wrapper object" do
+        expect(io_wrapper).not_to be_closed
+        io_wrapper.close
+        expect(io_wrapper).to be_closed
       end
     end
 
@@ -57,8 +74,8 @@ require 'spec_helper'
       it "should read data from the wrapped io object" do
         expect(io_wrapper.read()).to eq(expected_data)
       end
-      it "should raise an IOError if the wrapped io is closed" do
-        wrapped_io.to_io.close
+      it "should raise an IOError if the real io object is closed" do
+        real_io_object.close
         expect { io_wrapper.read() }.to raise_error(IOError)
       end
       it "should raise an IOError if #close was called before #read is called" do
