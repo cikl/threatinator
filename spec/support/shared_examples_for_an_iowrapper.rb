609b87e4caa8dc9d7f1468dfba63541242a56b86
justfalter <falter@gmail.com>
2014-06-16 10:40:48 -0500

    Switch to StringIO because it is file-like

--- spec/support/shared_examples_for_an_iowrapper.rb
+++ spec/support/shared_examples_for_an_iowrapper.rb
@@ -1,58 +1,54 @@
 require 'spec_helper'
-  shared_examples_for "an iowrapper" do
+require 'stringio'
 
-    # The real IO object that is ultimately wrapped.
-    let(:pipe) { 
-      r, w = IO.pipe()
-      w.write(input_data)
-      w.close
-      r
-    }
+shared_examples_for "an iowrapper" do
 
-    let!(:io_wrapper) { described_class.new(pipe) }
+  # The real IO object that is ultimately wrapped.
+  let(:source_io) { StringIO.new(input_data) }
 
-    after :each do
-      io_wrapper.close rescue nil
-      pipe.close rescue nil
+  let!(:io_wrapper) { described_class.new(source_io) }
+
+  after :each do
+    io_wrapper.close rescue nil
+  end
+
+  describe "#close" do
+    it "should close the real io object" do
+      expect(source_io).not_to be_closed
+      io_wrapper.close
+      expect(source_io).to be_closed
     end
 
-    describe "#close" do
-      it "should close the real io object" do
-        expect(pipe).not_to be_closed
-        io_wrapper.close
-        expect(pipe).to be_closed
-      end
-
-      it "should close the io wrapper object" do
-        expect(io_wrapper).not_to be_closed
-        io_wrapper.close
-        expect(io_wrapper).to be_closed
-      end
-    end
-
-    describe "#closed?" do
-      it "should return true if the io is closed" do
-        io_wrapper.close
-        expect(io_wrapper).to be_closed
-      end
-      it "should return false if the io is not closed" do
-        expect(io_wrapper).not_to be_closed
-      end
-    end
-
-    describe "#read" do
-      it "should read data from the wrapped io object" do
-        expect(io_wrapper.read()).to eq(expected_data)
-      end
-      it "should raise an IOWrapperError if it has to read from a closed IO" do
-        pipe.close
-        expect { 
-          io_wrapper.read() 
-        }.to raise_error(Threatinator::Exceptions::IOWrapperError)
-      end
-      it "should raise an IOWrapperError if #close was called before #read is called" do
-        io_wrapper.close
-        expect { io_wrapper.read() }.to raise_error(Threatinator::Exceptions::IOWrapperError)
-      end
+    it "should close the io wrapper object" do
+      expect(io_wrapper).not_to be_closed
+      io_wrapper.close
+      expect(io_wrapper).to be_closed
     end
   end
+
+  describe "#closed?" do
+    it "should return true if the io is closed" do
+      io_wrapper.close
+      expect(io_wrapper).to be_closed
+    end
+    it "should return false if the io is not closed" do
+      expect(io_wrapper).not_to be_closed
+    end
+  end
+
+  describe "#read" do
+    it "should read data from the wrapped io object" do
+      expect(io_wrapper.read()).to eq(expected_data)
+    end
+    it "should raise an IOWrapperError if it has to read from a closed IO" do
+      source_io.close
+      expect { 
+        io_wrapper.read() 
+      }.to raise_error(Threatinator::Exceptions::IOWrapperError)
+    end
+    it "should raise an IOWrapperError if #close was called before #read is called" do
+      io_wrapper.close
+      expect { io_wrapper.read() }.to raise_error(Threatinator::Exceptions::IOWrapperError)
+    end
+  end
+end
