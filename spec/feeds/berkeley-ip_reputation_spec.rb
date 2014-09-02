8b385725ae56263b1d677a6cce113581a0874cae
Pierre Lamy <pierre@userid.org>
2014-09-02 14:55:30 -0400

    Add berkley feed, fix #16

---
+++ spec/feeds/berkeley-ip_reputation_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'feeds/berkeley-ip_reputation.feed', :feed do
+  let(:provider) { 'berkeley' }
+  let(:name) { 'ip_reputation' }
+
+  it_fetches_url 'https://security.berkeley.edu/aggressive_ips/ips'
+
+  describe_parsing_the_file feed_data('berkeley.txt') do
+    it "should have parsed 16 records" do
+      expect(num_records_parsed).to eq(16)
+    end
+    it "should have filtered 13 records" do
+      expect(num_records_filtered).to eq(13)
+    end
+    it "should have missed 0 records" do
+      expect(num_records_missed).to eq(0)
+    end
+  end
+
+  describe_parsing_a_record 'HOSTILE_IP: 116.10.191.182      LAST_SEEN: 1403615662' do
+    it "should have parsed" do
+      expect(status).to eq(:parsed)
+    end
+    it "should have parsed 1 event" do
+      expect(events.count).to eq(1)
+    end
+	describe 'event 0' do
+      subject { events[0] }
+      its(:type) { is_expected.to be(:scanning) }
+      its(:ipv4s) { is_expected.to match_array(['116.10.191.182']) }
+    end
+  end
+
+  describe_parsing_a_record 'HOSTILE_IP: 144.0.0.22  LAST_SEEN: 1404389169' do
+    it "should have parsed" do
+      expect(status).to eq(:parsed)
+    end
+    it "should have parsed 1 event" do
+      expect(events.count).to eq(1)
+    end
+	describe 'event 0' do
+      subject { events[0] }
+      its(:type) { is_expected.to be(:scanning) }
+      its(:ipv4s) { is_expected.to match_array(['144.0.0.22']) }
+    end
+  end
+end
+
+
