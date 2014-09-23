6eea668c99f7a53754cd34ab1a6bc5a8d1f9d220
justfalter <falter@gmail.com>
2014-09-23 17:11:34 -0500

    Update CSV parser to include URLs

--- lib/threatinator/plugins/output/csv.rb
+++ lib/threatinator/plugins/output/csv.rb
@@ -22,13 +22,18 @@ module Threatinator
                              :fqdn_1,
                              :fqdn_2,
                              :fqdn_3,
-                             :fqdn_4
+                             :fqdn_4,
+                             :url_1,
+                             :url_2,
+                             :url_3,
+                             :url_4
                            ])
         end
 
         def handle_event(event)
           ipv4s = event.ipv4s.to_a[0..3]
           fqdns = event.fqdns.to_a[0..3]
+          urls  = event.urls.to_a[0..3].map {|x| x.to_s }
           @csv.add_row([
             event.feed_provider,
             event.feed_name,
@@ -40,7 +45,11 @@ module Threatinator
             fqdns[0],
             fqdns[1],
             fqdns[2],
-            fqdns[3]
+            fqdns[3],
+            urls[0],
+            urls[1],
+            urls[2],
+            urls[3],
           ])
         end
       end
--- spec/threatinator/plugins/output/csv_spec.rb
+++ spec/threatinator/plugins/output/csv_spec.rb
@@ -24,7 +24,8 @@ describe Threatinator::Plugins::Output::Csv do
         ret << build(:event, feed_provider: "my_prov#{i}", 
               feed_name: "my_name#{i}", type: :scanning,
               ipv4s: ["#{i}.1.1.1","#{i}.1.1.2","#{i}.1.1.3","#{i}.1.1.4","#{i}.1.1.5"],
-              fqdns: ["a#{i}.com","b#{i}.com","c#{i}.com","d#{i}.com","e#{i}.com"]
+              fqdns: ["a#{i}.com","b#{i}.com","c#{i}.com","d#{i}.com","e#{i}.com"],
+              urls:  ["http://a#{i}.com","http://b#{i}.com","http://c#{i}.com","http://d#{i}.com","http://e#{i}.com"],
              ) 
       end
       ret
@@ -33,13 +34,13 @@ describe Threatinator::Plugins::Output::Csv do
 
     describe "the header" do
       subject { lines.first }
-      it { is_expected.to eq("provider,feed_name,type,ipv4_1,ipv4_2,ipv4_3,ipv4_4,fqdn_1,fqdn_2,fqdn_3,fqdn_4\n") }
+      it { is_expected.to eq("provider,feed_name,type,ipv4_1,ipv4_2,ipv4_3,ipv4_4,fqdn_1,fqdn_2,fqdn_3,fqdn_4,url_1,url_2,url_3,url_4\n") }
     end
 
     it "outputs each event, limited to the first four IPs and four fqdns" do
       line2 = lines[1]
       1.upto(10) do |i|
-        expect(lines[i]).to eq("my_prov#{i},my_name#{i},scanning,#{i}.1.1.1,#{i}.1.1.2,#{i}.1.1.3,#{i}.1.1.4,a#{i}.com,b#{i}.com,c#{i}.com,d#{i}.com\n")
+        expect(lines[i]).to eq("my_prov#{i},my_name#{i},scanning,#{i}.1.1.1,#{i}.1.1.2,#{i}.1.1.3,#{i}.1.1.4,a#{i}.com,b#{i}.com,c#{i}.com,d#{i}.com,http://a#{i}.com,http://b#{i}.com,http://c#{i}.com,http://d#{i}.com\n")
       end
     end
   end
