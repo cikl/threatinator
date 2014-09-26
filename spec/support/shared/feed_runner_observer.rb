f52cbff20830a3ab3ad47a24b878927986a69697
justfalter <falter@gmail.com>
2014-09-26 15:20:44 -0500

    Expand messages that observer tester looks for

--- spec/support/shared/feed_runner_observer.rb
+++ spec/support/shared/feed_runner_observer.rb
@@ -36,6 +36,54 @@ shared_examples_for "a FeedRunner observer" do
 
     let(:record) { build(:record, line_number: 23, pos_start: 99, pos_end: 105, data: "foobar\r\n") }
 
+    describe "#update(:start_fetch)" do
+      it "does not raise any errors when handling an :start_fetch event" do
+        expect {
+          observer.update(:start_fetch)
+        }.not_to raise_error
+      end
+    end
+
+    describe "#update(:end_fetch)" do
+      it "does not raise any errors when handling an :end_fetch event" do
+        expect {
+          observer.update(:end_fetch)
+        }.not_to raise_error
+      end
+    end
+
+    describe "#update(:start_decode)" do
+      it "does not raise any errors when handling an :start_decode event" do
+        expect {
+          observer.update(:start_decode)
+        }.not_to raise_error
+      end
+    end
+
+    describe "#update(:end_decode)" do
+      it "does not raise any errors when handling an :end_decode event" do
+        expect {
+          observer.update(:end_decode)
+        }.not_to raise_error
+      end
+    end
+
+    describe "#update(:start_parse_record, record)" do
+      it "does not raise any errors when handling an :start_parse_record event" do
+        expect {
+          observer.update(:start_parse_record, record)
+        }.not_to raise_error
+      end
+    end
+
+    describe "#update(:end_parse_record, record)" do
+      it "does not raise any errors when handling an :end_parse_record event" do
+        expect {
+          observer.update(:end_parse_record, record)
+        }.not_to raise_error
+      end
+    end
+
     describe "#update(:record_filtered, record)" do
       it "does not raise any errors when handling a :record_filtered event" do
         expect {
@@ -60,10 +108,15 @@ shared_examples_for "a FeedRunner observer" do
       end
     end
 
-    describe "#update(:record_error, record)" do
+    describe "#update(:record_error, record, array_of_errors)" do
       it "does not raise any errors when handling a :record_error event" do
+        errors = [
+          Threatinator::Exceptions::EventBuildError.new("error 1"),
+          Threatinator::Exceptions::EventBuildError.new("error 2"),
+          Threatinator::Exceptions::EventBuildError.new("error 3")
+        ]
         expect {
-          observer.update(:record_missed, record)
+          observer.update(:record_missed, record, errors)
         }.not_to raise_error
       end
     end
