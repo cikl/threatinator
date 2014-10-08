b1e91eb04fe3a6432c4730a25053e973b239bca8
Pierre Lamy <pierre@userid.org>
2014-10-08 14:02:45 -0400

    Add chaosreigns IP rep feed

---
+++ spec/feeds/chaosreigns-ip_reputation_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'feeds/chaosreigns-ip_reputation.feed', :feed do
+  let(:provider) { 'chaosreigns' }
+  let(:name) { 'ip_reputation' }
+
+  it_fetches_url 'http://www.chaosreigns.com/iprep/iprep.txt'
+
+  describe_parsing_the_file feed_data('chaosreigns-ip-reputation.txt') do
+    it "should have parsed 5 records" do
+      expect(num_records_parsed).to eq(5)
+    end
+    it "should have filtered 21 records" do
+      expect(num_records_filtered).to eq(21)
+    end
+    it "should have missed 0 records" do
+      expect(num_records_missed).to eq(0)
+    end
+  end
+
+  describe_parsing_a_record '       5.9.94.3     0    17' do
+    it "should have parsed" do
+      expect(status).to eq(:parsed)
+    end
+    it "should have parsed 1 event" do
+      expect(events.count).to eq(1)
+    end
+	describe 'event 0' do
+      subject { events[0] }
+      its(:type) { is_expected.to be(:scanning) }
+	  its(:ipv4s) { is_expected.to eq(build(:ipv4s, values: ['5.9.94.3'])) }
+    end
+  end
+
+  describe_parsing_a_record '       1.52.9.7     0     0' do
+    it "should have parsed" do
+      expect(status).to eq(:parsed)
+    end
+    it "should have parsed 1 event" do
+      expect(events.count).to eq(1)
+    end
+	describe 'event 0' do
+      subject { events[0] }
+      its(:type) { is_expected.to be(:scanning) }
+	  its(:ipv4s) { is_expected.to eq(build(:ipv4s, values: ['1.52.9.7'])) }
+    end
+  end
+end
+
+
